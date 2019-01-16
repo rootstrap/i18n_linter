d1859bce8c54f805253c28107cecad0e8c89b42f
Franco Pariani <franco@rootstrap.com>
2019-01-16 11:38:55 -0300

    Fix in strftime test

--- spec/fixtures/negative_rules/sample_controller.rb
+++ spec/fixtures/negative_rules/sample_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
   def update_published_date
     # Testing Strftime rule
-    Video.find(video_params[:id]).update(published_date: DateTime.now.strftime('%Y-%m-%d'))
+    Video.find(video_params[:id]).update(published_date: DateTime.now.strftime('%Y-%m-%d %I:%M%p'))
   end
 
   def video_params
